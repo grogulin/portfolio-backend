67da7a6dde5ac402d7ee5cf0ac0e3fdeb7a37dff
Yaroslav Grogul <time7bass@gmail.com>
2023-08-21 11:21:27 +0200

    disabled registration of new admins

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -7,22 +7,23 @@ const router = express.Router();
 const sequelize = require('../config/database'); // Import your Sequelize instance
 
 router.post('/signup', async (req, res, next) => {
-  const transaction = await sequelize.transaction(); // Start a transaction
+  res.status(403);
+  // const transaction = await sequelize.transaction(); // Start a transaction
 
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // try {
+  //   const { username, password } = req.body;
+  //   const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user within the transaction
-    await User.create({ username, password: hashedPassword }, { transaction });
+  //   // Create a new user within the transaction
+  //   await User.create({ username, password: hashedPassword }, { transaction });
 
-    await transaction.commit(); // Commit the transaction
+  //   await transaction.commit(); // Commit the transaction
 
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    await transaction.rollback(); // Rollback the transaction in case of an error
-    res.status(500).json({ error: error});
-  }
+  //   res.status(201).json({ message: 'User registered successfully' });
+  // } catch (error) {
+  //   await transaction.rollback(); // Rollback the transaction in case of an error
+  //   res.status(500).json({ error: error});
+  // }
 });
 
 module.exports = router;
